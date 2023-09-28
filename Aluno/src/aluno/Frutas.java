177aa63f5abd00818c9f728289ef2c37ed1d4f68
Thiago Teruel <thiteruel@gmail.com>
2023-09-28 16:38:10 -0300

    feat(Frutas.java): Exercicos java

---
+++ Aluno/src/aluno/Frutas.java
@@ -0,0 +1,15 @@
+package aluno;
+
+public class Frutas {
+
+	public static void main(String[] args) {
+		String s1 = "Eu como maça";
+		String s2 = "Banana é minha fruta predileta.";
+		System.out.println(s1.substring(7)+ " " + s2.substring(7, s2.length()));
+		
+		System.out.println(s1.indexOf("como"));
+		System.out.println(s1.charAt(4));
+		
+	}
+
+}
